ef99595875c22114aa1e4dee38a1f5864a33bf97
mairi tikk <mairi.tikk@gmail.com>
2023-09-02 23:18:38 +0200

    add validators

--- practica6/src/app/pages/newuser/newuser.component.ts
+++ practica6/src/app/pages/newuser/newuser.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/interfaces/user.interface';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
@@ -10,44 +11,44 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class NewuserComponent {
   userForm: FormGroup;
-  useserService = inject(UsersService);
+  userService = inject(UsersService);
   //Router, redireccion de ruta
   router = inject(Router);
 
   //valor o variale de la rutta, usar ruter o ActiveRoute
   activeRoute = inject(ActivatedRoute);
 
+  titulo = 'New User';
+
   ngOnInit(): void {
-    this.activeRoute.params.subscribe((params: any) => {
-      let id: number = Number(params.id);
-      
-      //llamar servicio funcion getById (id)
-      let user = this.useserService.getById(id);
-      
-      this.userForm= new FormGroup(
-        {
-          first_name: new FormControl(user?.first_name, [Validators.required]),
-          last_name: new FormControl(user?.last_name, []),
-          username: new FormControl(user?.username, []),
-          password: new FormControl(user?.password, []),
-          email: new FormControl(user?.email, []),
-          image: new FormControl(user?.image, []),
-        },
-        []
-      );
-      
-    });
+    
   }
+  
 
   constructor() {
-    this.userForm = new FormGroup(
+
+    let user: User | undefined;
+
+    this.activeRoute.params.subscribe((params: any) => {
+      let id: number = Number(params.id);
+
+      if (!Number.isNaN(id)){
+        this.titulo= "Update User";
+      }
+      
+      //llamar servicio funcion getById (id)
+      user = this.userService.getById(id);
+      
+    });
+
+    this.userForm= new FormGroup(
       {
-        first_name: new FormControl('', [Validators.required]),
-        last_name: new FormControl('', []),
-        username: new FormControl('', []),
-        password: new FormControl('', []),
-        email: new FormControl('', []),
-        image: new FormControl('', []),
+        first_name: new FormControl(user?.first_name, [Validators.required, Validators.minLength(3)]),
+        last_name: new FormControl(user?.last_name, [Validators.required, Validators.minLength(3)]),
+        username: new FormControl(user?.username, [Validators.required, Validators.minLength(3)]),
+        password: new FormControl(user?.password, [Validators.required]),
+        email: new FormControl(user?.email, [Validators.required, Validators.email]),
+        image: new FormControl(user?.image, [Validators.required]),
       },
       []
     );
@@ -55,12 +56,16 @@ export class NewuserComponent {
 
 
   getDataForm(): void {
-    let response = this.useserService.insert(this.userForm.value);
+    let response = this.userService.insert(this.userForm.value);
     if (response === 'ok') {
-      this.router.navigate(['/user']);
+      this.router.navigate(['']);
     } else {
       alert('Usario no se ha podido registrar');
     }
     this.userForm.reset();
   }
+ checkControl(formcontrolName: string, valiador: string): boolean | undefined{
+  return this.userForm.get(formcontrolName)?.hasError(valiador) && this.userForm.get(formcontrolName)?.touched
 }
+}
+
